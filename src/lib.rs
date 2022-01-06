71a1dbb513f75904fe7f2af0904b2f162d01b508
Zach Mitchell <zmitchell@fastmail.com>
2022-01-06 18:07:02 -0500

    test: Bundle validation data separately

--- src/lib.rs
+++ src/lib.rs
@@ -419,264 +419,123 @@ mod test {
     use super::*;
     use numpy::ndarray::Array2;
 
-    // These are known-good results we'll test against
-    const BRIXNER_HAM: [f64; 49] = [
-        // 7x7 matrix unraveled
-        1.242000000000000000e+04,
-        -1.060000000000000000e+02,
-        8.000000000000000000e+00,
-        -5.000000000000000000e+00,
-        6.000000000000000000e+00,
-        -8.000000000000000000e+00,
-        -4.000000000000000000e+00,
-        -1.060000000000000000e+02,
-        1.256000000000000000e+04,
-        2.800000000000000000e+01,
-        6.000000000000000000e+00,
-        2.000000000000000000e+00,
-        1.300000000000000000e+01,
-        1.000000000000000000e+00,
-        8.000000000000000000e+00,
-        2.800000000000000000e+01,
-        1.214000000000000000e+04,
-        -6.200000000000000000e+01,
-        -1.000000000000000000e+00,
-        -9.000000000000000000e+00,
-        1.700000000000000000e+01,
-        -5.000000000000000000e+00,
-        6.000000000000000000e+00,
-        -6.200000000000000000e+01,
-        1.231500000000000000e+04,
-        -7.000000000000000000e+01,
-        -1.900000000000000000e+01,
-        -5.700000000000000000e+01,
-        6.000000000000000000e+00,
-        2.000000000000000000e+00,
-        -1.000000000000000000e+00,
-        -7.000000000000000000e+01,
-        1.246000000000000000e+04,
-        4.000000000000000000e+01,
-        -2.000000000000000000e+00,
-        -8.000000000000000000e+00,
-        1.300000000000000000e+01,
-        -9.000000000000000000e+00,
-        -1.900000000000000000e+01,
-        4.000000000000000000e+01,
-        1.250000000000000000e+04,
-        3.200000000000000000e+01,
-        -4.000000000000000000e+00,
-        1.000000000000000000e+00,
-        1.700000000000000000e+01,
-        -5.700000000000000000e+01,
-        -2.000000000000000000e+00,
-        3.200000000000000000e+01,
-        1.240000000000000000e+04,
-    ];
-    const BRIXNER_DIPOLE_MOMENTS: [f64; 21] = [
-        // 7x3 matrix unraveled, one dipole moment per row
-        -7.409999999999999920e-01,
-        -5.605999999999999872e-01,
-        -3.695999999999999841e-01,
-        -8.570999999999999730e-01,
-        5.038000000000000256e-01,
-        -1.073000000000000065e-01,
-        -1.970999999999999974e-01,
-        9.574000000000000288e-01,
-        -2.109999999999999931e-01,
-        -7.992000000000000215e-01,
-        -5.335999999999999632e-01,
-        -2.766000000000000125e-01,
-        -7.368999999999999995e-01,
-        6.558000000000000496e-01,
-        1.640999999999999959e-01,
-        -1.350000000000000089e-01,
-        -8.791999999999999815e-01,
-        4.568999999999999728e-01,
-        -4.950999999999999845e-01,
-        -7.083000000000000407e-01,
-        -5.030999999999999917e-01,
-    ];
-    const BRIXNER_PIG_POS: [f64; 21] = [
-        // 7x3 matrix unraveled, one position vector per row
-        2.625349998474121094e+01,
-        2.782999992370605469e+00,
-        -1.110700035095214844e+01,
-        1.554899978637695312e+01,
-        -1.585500001907348633e+00,
-        -1.687949943542480469e+01,
-        3.403500080108642578e+00,
-        -1.370650005340576172e+01,
-        -1.425450038909912109e+01,
-        6.876500129699707031e+00,
-        -2.091850090026855469e+01,
-        -6.376999855041503906e+00,
-        1.937750053405761719e+01,
-        -1.849099922180175781e+01,
-        -1.491500020027160645e+00,
-        2.174550056457519531e+01,
-        -7.323999881744384766e+00,
-        3.115000128746032715e-01,
-        1.058049964904785156e+01,
-        -8.205499649047851562e+00,
-        -5.826499938964843750e+00,
-    ];
-    const BRIXNER_E_VALS: [f64; 7] = [
-        1.211586132812500000e+04,
-        1.262016503906250000e+04,
-        1.254819335937500000e+04,
-        1.227897460937500000e+04,
-        1.236427050781250000e+04,
-        1.245148828125000000e+04,
-        1.241604589843750000e+04,
-    ];
-    const BRIXNER_E_VECS: [f64; 49] = [
-        // 7x7 matrix unraveled, one eigenvector per column
-        -4.62779193e-02,
-        -4.64960172e-01,
-        7.95694100e-02,
-        -6.59153288e-02,
-        8.77070174e-01,
-        3.97137859e-02,
-        -1.28451052e-02,
-        -7.58944119e-02,
-        8.71062859e-01,
-        -1.11174889e-01,
-        -3.56131041e-02,
-        4.60743931e-01,
-        9.78584139e-02,
-        -3.80231272e-04,
-        9.39964910e-01,
-        4.25812316e-02,
-        3.40729894e-02,
-        2.84851912e-01,
-        8.92114997e-02,
-        7.28455406e-02,
-        1.38152312e-01,
-        3.21242706e-01,
-        -7.31556453e-03,
-        -3.04078678e-01,
-        -7.86447530e-01,
-        -1.14269838e-02,
-        -2.64925617e-01,
-        -3.39808606e-01,
-        6.55948819e-02,
-        3.18615043e-02,
-        5.60014918e-01,
-        -3.18976754e-01,
-        -9.03763572e-02,
-        7.11933630e-01,
-        -2.53578190e-01,
-        3.22989047e-02,
-        1.43941356e-01,
-        7.10271801e-01,
-        6.43476101e-02,
-        4.29799612e-02,
-        -6.29269202e-01,
-        -2.67925391e-01,
-        4.64504760e-03,
-        3.82176052e-02,
-        2.63782679e-01,
-        -4.34579985e-01,
-        -1.89343423e-02,
-        -1.02592731e-01,
-        8.53927980e-01,
-    ];
-    const BRIXNER_EXCITON_DIPOLE_MOMENTS: [f64; 21] = [
-        // 7x3 matrix unraveled, one dipole moment per row
-        -3.97660199e-01,
-        7.27544933e-01,
-        -2.38755973e-01,
-        -4.66431013e-01,
-        6.11441002e-01,
-        1.23191081e-01,
-        -3.66529533e-01,
-        -3.49789113e-01,
-        3.43151541e-01,
-        1.09327940e+00,
-        7.53429504e-01,
-        3.81304753e-01,
-        -9.83093328e-01,
-        -2.51699821e-01,
-        -3.74933231e-01,
-        -3.04810553e-01,
-        1.33095014e+00,
-        -8.63407929e-02,
-        5.44412889e-02,
-        -2.14975460e-01,
-        -5.24009187e-01,
-    ];
-    const BRIXNER_STICK_ABS: [f64; 7] = [
-        7.44459679e-01,
-        6.06594031e-01,
-        3.74449303e-01,
-        1.90830917e+00,
-        1.17040022e+00,
-        1.87179248e+00,
-        3.23763930e-01,
-    ];
-    const BRIXNER_STICK_CD: [f64; 7] = [
-        -3.68370364e-03,
-        -2.10216543e-03,
-        3.52796169e-04,
-        6.69710466e-03,
-        -2.78332787e-03,
-        6.38344482e-03,
-        -4.84810079e-03,
-    ];
+    fn load_ham() -> Array2<f64> {
+        let contents = include_str!("../validation_data/hamiltonian.txt");
+        let data: Vec<f64> = contents
+            .split("\n")
+            .map(|line| line.parse::<f64>().unwrap())
+            .collect();
+        Array2::from_shape_vec((7, 7), data).unwrap()
+    }
 
-    macro_rules! brixner_ham {
-        () => {
-            Array2::from_shape_vec((7, 7), Vec::from(BRIXNER_HAM)).unwrap()
-        };
+    fn load_dipole_moments() -> Array2<f64> {
+        let contents = include_str!("../validation_data/dipole_moments.txt");
+        let data: Vec<f64> = contents
+            .split("\n")
+            .map(|line| line.parse::<f64>().unwrap())
+            .collect();
+        Array2::from_shape_vec((7, 3), data).unwrap()
     }
-    macro_rules! brixner_e_vals {
-        () => {
-            Array1::from_vec(Vec::from(BRIXNER_E_VALS))
-        };
+
+    fn load_positions() -> Array2<f64> {
+        let contents = include_str!("../validation_data/positions.txt");
+        let data: Vec<f64> = contents
+            .split("\n")
+            .map(|line| line.parse::<f64>().unwrap())
+            .collect();
+        Array2::from_shape_vec((7, 3), data).unwrap()
     }
-    macro_rules! brixner_e_vecs {
-        () => {
-            Array2::from_shape_vec((7, 7), Vec::from(BRIXNER_E_VECS)).unwrap()
-        };
+
+    fn load_eigenvalues() -> Array1<f64> {
+        let contents = include_str!("../validation_data/eigenvalues.txt");
+        let data: Vec<f64> = contents
+            .split("\n")
+            .map(|line| line.parse::<f64>().unwrap())
+            .collect();
+        Array1::from_vec(data)
     }
-    macro_rules! brixner_dipole_moments {
-        () => {
-            Array2::from_shape_vec((7, 3), Vec::from(BRIXNER_DIPOLE_MOMENTS)).unwrap()
-        };
+
+    fn load_eigenvectors() -> Array2<f64> {
+        let contents = include_str!("../validation_data/eigenvectors.txt");
+        let data: Vec<f64> = contents
+            .split("\n")
+            .map(|line| line.parse::<f64>().unwrap())
+            .collect();
+        Array2::from_shape_vec((7, 7), data).unwrap()
     }
-    macro_rules! brixner_pig_pos {
-        () => {
-            Array2::from_shape_vec((7, 3), Vec::from(BRIXNER_PIG_POS)).unwrap()
-        };
+
+    fn load_exciton_dipole_moments() -> Array2<f64> {
+        let contents = include_str!("../validation_data/exciton_dipole_moments.txt");
+        let data: Vec<f64> = contents
+            .split("\n")
+            .map(|line| line.parse::<f64>().unwrap())
+            .collect();
+        Array2::from_shape_vec((7, 3), data).unwrap()
     }
-    macro_rules! brixner_exciton_dipole_moments {
-        () => {
-            Array2::from_shape_vec((7, 3), Vec::from(BRIXNER_EXCITON_DIPOLE_MOMENTS)).unwrap()
-        };
+
+    fn load_dipole_strengths() -> Array1<f64> {
+        let contents = include_str!("../validation_data/dipole_strengths.txt");
+        let data: Vec<f64> = contents
+            .split("\n")
+            .map(|line| line.parse::<f64>().unwrap())
+            .collect();
+        Array1::from_vec(data)
     }
-    macro_rules! brixner_stick_abs {
-        () => {
-            Array1::from_vec(Vec::from(BRIXNER_STICK_ABS))
-        };
+
+    fn load_rotational_strengths() -> Array1<f64> {
+        let contents = include_str!("../validation_data/rotational_strengths.txt");
+        let data: Vec<f64> = contents
+            .split("\n")
+            .map(|line| line.parse::<f64>().unwrap())
+            .collect();
+        Array1::from_vec(data)
     }
-    macro_rules! brixner_stick_cd {
-        () => {
-            Array1::from_vec(Vec::from(BRIXNER_STICK_CD))
-        };
+
+    fn load_x() -> Array1<f64> {
+        let contents = include_str!("../validation_data/x.txt");
+        let data: Vec<f64> = contents
+            .split("\n")
+            .map(|line| line.parse::<f64>().unwrap())
+            .collect();
+        Array1::from_vec(data)
     }
+
+    fn load_abs() -> Array1<f64> {
+        let contents = include_str!("../validation_data/abs.txt");
+        let data: Vec<f64> = contents
+            .split("\n")
+            .map(|line| line.parse::<f64>().unwrap())
+            .collect();
+        Array1::from_vec(data)
+    }
+
+    fn load_cd() -> Array1<f64> {
+        let contents = include_str!("../validation_data/cd.txt");
+        let data: Vec<f64> = contents
+            .split("\n")
+            .map(|line| line.parse::<f64>().unwrap())
+            .collect();
+        Array1::from_vec(data)
+    }
+
+    const CONFIG: BroadeningConfig = BroadeningConfig {
+        x_from: 11790.0,
+        x_to: 13300.0,
+        x_step: 1.0,
+        bw: 120.0,
+    };
 
     #[test]
     fn correctly_loads_hamiltonian() {
-        let ham = brixner_ham!();
+        let ham = load_ham();
         assert_relative_eq!(ham[[1, 2]], 28.0);
     }
 
     #[test]
     fn diagonalizes_brixner_hamiltonian() {
-        let ham = brixner_ham!();
-        let good_e_vals = brixner_e_vals!();
-        // The 6th eigenvector has its sign flipped for some reason
-        let good_e_vecs = brixner_e_vecs!();
+        let ham = load_ham();
+        let good_e_vals = load_eigenvalues();
+        let good_e_vecs = load_eigenvectors();
         let (test_e_vals, test_e_vecs) = diagonalize(&ham);
         println!("{}", test_e_vecs);
         assert_abs_diff_eq!(test_e_vals, good_e_vals, epsilon = 1.0);
@@ -685,10 +544,10 @@ mod test {
 
     #[test]
     fn computes_brixner_exciton_dipole_moments() {
-        let e_vecs = brixner_e_vecs!();
-        let dipole_moments = brixner_dipole_moments!();
+        let e_vecs = load_eigenvectors();
+        let dipole_moments = load_dipole_moments();
         let test_exc_dipole_moments = exciton_dipole_moments(&e_vecs, &dipole_moments);
-        let good_exc_dipole_moments = brixner_exciton_dipole_moments!();
+        let good_exc_dipole_moments = load_exciton_dipole_moments();
         assert_abs_diff_eq!(
             test_exc_dipole_moments,
             good_exc_dipole_moments,
@@ -698,19 +557,19 @@ mod test {
 
     #[test]
     fn computes_brixner_stick_abs() {
-        let exciton_dpm = brixner_exciton_dipole_moments!();
+        let exciton_dpm = load_exciton_dipole_moments();
         let test_stick_abs = stick_abs_single(&exciton_dpm);
-        let good_stick_abs = brixner_stick_abs!();
+        let good_stick_abs = load_dipole_strengths();
         assert_abs_diff_eq!(test_stick_abs, good_stick_abs, epsilon = 1e-4);
     }
 
     #[test]
     fn computes_brixner_stick_cd() {
-        let dipole_moments = brixner_dipole_moments!();
-        let e_vecs = brixner_e_vecs!();
-        let e_vals = brixner_e_vals!();
-        let pig_pos = brixner_pig_pos!();
-        let good_stick_cd = brixner_stick_cd!();
+        let dipole_moments = load_dipole_moments();
+        let e_vecs = load_eigenvectors();
+        let e_vals = load_eigenvalues();
+        let pig_pos = load_positions();
+        let good_stick_cd = load_rotational_strengths();
         let test_stick_cd = stick_cd_single(
             e_vecs.view(),
             dipole_moments.view(),
---
+++ validation_data/README.md
@@ -0,0 +1,17 @@
+# Validation data
+This validation data comes from the Brixner Hamiltonian. The bandwidth for the broadened spectra is 120cm^-1.
+
+Input data:
+- Hamiltonian (7x7)
+- Dipole moments (7x3)
+- Positions (7x3)
+
+Computed data:
+- Eigenvalues (7x1)
+- Eigenvectors (7x7, one per column)
+- Exciton dipole moments (7x3)
+- Dipole strengths (7x1)
+- Rotational strengths (7x1)
+- x (x-axis for broadened spectra) (1510x1)
+- absorption (1510x1)
+- cd (circular dichroism) (1510x1)
