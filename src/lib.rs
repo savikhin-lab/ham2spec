9416025db8f3dae875bbc884028f22151cb406e9
Zach Mitchell <zmitchell@fastmail.com>
2022-01-07 16:47:52 -0500

    refactor: Import directly from ndarray

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,7 @@ extern crate lapack_src;
 #[cfg(test)]
 use approx::{assert_abs_diff_eq, assert_relative_eq};
 use lapack::dgeev;
-use numpy::ndarray::{
-    arr1, arr2, Array, Array1, Array2, ArrayView1, ArrayView2, ArrayView3, Axis, Zip,
-};
+use ndarray::{arr1, arr2, Array, Array1, Array2, ArrayView1, ArrayView2, ArrayView3, Axis, Zip};
 use numpy::{
     IntoPyArray, PyArray1, PyArray2, PyReadonlyArray1, PyReadonlyArray2, PyReadonlyArray3,
     ToPyArray,
